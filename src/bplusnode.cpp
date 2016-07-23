b8383d0ec72c8ddc7d55d4576b59cfdbc68419fb
Wuwei Lin <vincentl13x@gmail.com>
2016-07-23 21:09:17 +0800

    Add B+ node to a separate file

---
+++ src/bplusnode.cpp
@@ -0,0 +1,99 @@
+#include "bplusnode.h"
+#include "datafile.h"
+
+
+namespace naivedb {
+
+
+BPlusNode::BPlusNode(const Location &location, DBStore *db_store) : db_store_(db_store) {
+    data = db_store_->indexRecordAt(location)->getData();
+}
+
+bool BPlusNode::isLeaf() const { return data->is_leaf; }
+
+void BPlusNode::asLeaf() { data->is_leaf = true; }
+
+void BPlusNode::asInternal() { data->is_leaf = false; }
+
+void BPlusNode::init(bool is_leaf) { data->init(is_leaf); }
+
+Location BPlusNode::getChild(int i) const {
+    assert(!isLeaf());
+    assert(i >= 0 && i < TreeOrder);
+    return data->Internal.children[i];
+}
+
+Location BPlusNode::getValue(int i) const {
+    assert(isLeaf());
+    assert(i >= 0 && i < TreeOrder - 1);
+    return data->Leaf.values[i];
+}
+
+DataRecord *BPlusNode::getValueRec(int i) {
+    return db_store_->dataRecordAt(getValue(i));
+}
+
+IndexRecord *BPlusNode::getChildRec(int i) {
+    return db_store_->indexRecordAt(getChild(i));
+}
+
+const char *BPlusNode::getKey(int i) {
+    assert(i >= 0 && i < TreeOrder - 1);
+    Location loc = data->keys[i];
+    assert(!loc.isNull());
+    return reinterpret_cast<char *>(db_store_->dataRecordAt(loc)->getData());
+}
+
+int BPlusNode::find(const char *key) {
+    return linearSearch(0, TreeOrder - 1, key); // TODO: binary search would be better
+}
+
+int BPlusNode::linearSearch(int lo, int hi, const char *key) {
+    while (lo < hi) {
+        if (std::strcmp(getKey(lo), key) == 0) {
+            return lo;
+        }
+        ++lo;
+    }
+    return -1;
+}
+
+int BPlusNode::upperBound(const char *key) {
+    int i = 0;
+    while (i < TreeOrder - 1) {
+        if (data->keys[i].isNull() || strcmp(key, getKey(i)) < 0) {
+            break;
+        }
+        ++i;
+    }
+    return i;
+}
+
+
+void BPlusNode::addKey(int i, const Location &location) {
+    assert(i >= 0 && i < TreeOrder - 1);
+    data->keys[i] = location;
+}
+
+void BPlusNode::addChild(int i, const Location &location) {
+    assert(!isLeaf());
+    assert(i >= 0 && i < TreeOrder);
+    data->Internal.children[i] = location;
+}
+
+void BPlusNode::addValue(int i, const Location &location) {
+    assert(isLeaf());
+    assert(i >= 0 && i < TreeOrder - 1);
+    data->Leaf.values[i] = location;
+}
+
+Location BPlusNode::getNext() const {
+    assert(isLeaf());
+    return data->Leaf.next;
+}
+
+void BPlusNode::setNext(const Location &location) {
+    assert(isLeaf());
+    data->Leaf.next = location;
+}
+}
---
+++ src/bplusnode.h
@@ -0,0 +1,66 @@
+#ifndef NAIVEDB_BPLUSNODE_H
+#define NAIVEDB_BPLUSNODE_H
+
+
+#include "location.h"
+#include "dbstore.h"
+#include "bplusdata.h"
+
+
+namespace naivedb {
+
+class BPlus;
+class BPlusNodeData;
+class IndexRecord;
+class DataRecord;
+class DBStore;
+
+class BPlusNode {
+    friend class BPlus;
+public:
+    BPlusNode(const Location &location, DBStore *db_store);
+
+    bool isLeaf() const;
+
+    void asLeaf();
+
+    void asInternal();
+
+    void init(bool is_leaf);
+
+    const char * getKey(int i);
+
+    IndexRecord * getChildRec(int i);
+
+    DataRecord * getValueRec(int i);
+
+    Location getChild(int i)const;
+
+    Location getValue(int i)const;
+
+    int upperBound(const char *key);
+
+    int find(const char *key);
+
+    void addKey(int i, const Location &location);
+
+    void addChild(int i, const Location &location);
+
+    void addValue(int i, const Location &location);
+
+    Location getNext() const;
+
+    void setNext(const Location &location);
+private:
+
+    BPlusNodeData *data;
+
+    DBStore *db_store_;
+
+    int linearSearch(int lo, int hi, const char *key);
+};
+
+
+}
+
+#endif //NAIVEDB_BPLUSNODE_H
