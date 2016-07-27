c8a7b4493d44fb5e8587a675569353f3e52ba8fe
Wuwei Lin <vincentl13x@gmail.com>
2016-07-27 12:18:29 +0800

    Fix missing headers

--- benchmark/util.cpp
+++ benchmark/util.cpp
@@ -2,14 +2,15 @@
 #include <algorithm>
 #include <string>
 #include <iterator>
+#include <cstring>
 
 #include "util.h"
 
 std::string generateRandomString(size_t length)
 {
     const char* charmap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const size_t charmapLength = strlen(charmap);
-    auto generator = [&](){ return charmap[std::rand()%charmapLength]; };
+    const size_t charmap_length = std::strlen(charmap);
+    auto generator = [&](){ return charmap[std::rand() % charmap_length]; };
     std::string result;
     result.reserve(length);
     std::generate_n(std::back_inserter(result), length, generator);
